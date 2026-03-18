0b7d3a7a5ce40571beaf0f73f1a4fbe016791ba5
Villoh <mikelvillotatic@gmail.com>
2026-03-18 11:40:56 +0100

    Use glob in run_onchange scripts to auto-detect new files

--- .chezmoiscripts/run_onchange_install-ditto-themes.ps1
+++ .chezmoiscripts/run_onchange_install-ditto-themes.ps1
@@ -1,20 +1,8 @@
 # run_onchange_ triggers when this script's content changes.
-# The hashes below are updated by chezmoi when source files change.
-#
-# Classic.xml               {{ include "program_files/ditto/Themes/Classic.xml"               | sha256sum }}
-# DarkerDitto.xml           {{ include "program_files/ditto/Themes/DarkerDitto.xml"           | sha256sum }}
-# MonoDark.xml              {{ include "program_files/ditto/Themes/MonoDark.xml"              | sha256sum }}
-# Monolight.xml             {{ include "program_files/ditto/Themes/Monolight.xml"             | sha256sum }}
-# Nord.xml                  {{ include "program_files/ditto/Themes/Nord.xml"                  | sha256sum }}
-# Selenized Black.xml       {{ include "program_files/ditto/Themes/Selenized Black.xml"       | sha256sum }}
-# Selenized Dark.xml        {{ include "program_files/ditto/Themes/Selenized Dark.xml"        | sha256sum }}
-# Selenized Light.xml       {{ include "program_files/ditto/Themes/Selenized Light.xml"       | sha256sum }}
-# Selenized Strong.xml      {{ include "program_files/ditto/Themes/Selenized Strong.xml"      | sha256sum }}
-# Selenized White.xml       {{ include "program_files/ditto/Themes/Selenized White.xml"       | sha256sum }}
-# Selenized-Dark.xml        {{ include "program_files/ditto/Themes/Selenized-Dark.xml"        | sha256sum }}
-# Terminal.xml              {{ include "program_files/ditto/Themes/Terminal.xml"              | sha256sum }}
-# catppuccin-mocha-mauve.xml {{ include "program_files/ditto/Themes/catppuccin-mocha-mauve.xml" | sha256sum }}
-
+# Hashes are computed automatically for all .xml files in the source folder.
+{{ range (glob "program_files/ditto/Themes/*.xml") -}}
+# {{ . }} {{ include . | sha256sum }}
+{{ end }}
 $src = "{{ .chezmoi.sourceDir }}/program_files/ditto/Themes"
 $dst = "C:\Program Files\Ditto\Themes"
 
--- .chezmoiscripts/run_onchange_install-nilesoft-imports.ps1
+++ .chezmoiscripts/run_onchange_install-nilesoft-imports.ps1
@@ -1,15 +1,8 @@
 # run_onchange_ triggers when this script's content changes.
-# The hashes below are updated by chezmoi when source files change.
-#
-# develop.nss     {{ include "program_files/nilesoft/imports/develop.nss"     | sha256sum }}
-# file-manage.nss {{ include "program_files/nilesoft/imports/file-manage.nss" | sha256sum }}
-# goto.nss        {{ include "program_files/nilesoft/imports/goto.nss"        | sha256sum }}
-# images.nss      {{ include "program_files/nilesoft/imports/images.nss"      | sha256sum }}
-# modify.nss      {{ include "program_files/nilesoft/imports/modify.nss"      | sha256sum }}
-# taskbar.nss     {{ include "program_files/nilesoft/imports/taskbar.nss"     | sha256sum }}
-# terminal.nss    {{ include "program_files/nilesoft/imports/terminal.nss"    | sha256sum }}
-# theme.nss       {{ include "program_files/nilesoft/imports/theme.nss"       | sha256sum }}
-
+# Hashes are computed automatically for all .nss files in the source folder.
+{{ range (glob "program_files/nilesoft/imports/*.nss") -}}
+# {{ . }} {{ include . | sha256sum }}
+{{ end }}
 $src = "{{ .chezmoi.sourceDir }}/program_files/nilesoft/imports"
 $dst = "C:\Program Files\Nilesoft Shell\imports"
 
